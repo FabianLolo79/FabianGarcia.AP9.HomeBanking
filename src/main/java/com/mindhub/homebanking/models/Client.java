7653ded1909e45996a7e4cfd8c5fd13ab4d11615
Fabián Enrique García <fabianegl79@gmail.com>
2023-08-02 23:00:17 -0300

    comienzo del proyecto HomeBanking

---
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -0,0 +1,78 @@
+package com.mindhub.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private String name;
+    private String lastName;
+    private String dni;
+
+    private String email;
+
+    public Client(){  }
+
+    public Client(String name, String lastName, String dni, String email)
+    {
+        this.name = name;
+        this.lastName = lastName;
+        this.dni = dni;
+        this.email = email;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getDni()
+    {
+        return dni;
+    }
+
+    public void setDni(String dni)
+    {
+        this.dni = dni;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+
+}
